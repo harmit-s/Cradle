cb40e19226e8ea06d887425e57e9b65ffec38132
Harmit <162362409+harmit-s@users.noreply.github.com>
2024-05-08 19:27:12 -0400

    structuring, public folder assets, imports

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,6 +1,13 @@
+import { Level1 } from './Level1.jsx'
+import { Physics } from '@react-three/rapier'
+
 export default function Experience() {
 
     return <>
+        <Physics debug>
 
+            <Level1 />
+
+        </Physics>
     </>
 }
---
+++ src/Level1.jsx
@@ -0,0 +1,8 @@
+export default function Level1()
+{
+
+};
+
+return (
+    <></>
+)
